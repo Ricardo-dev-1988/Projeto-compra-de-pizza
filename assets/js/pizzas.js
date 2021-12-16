7d6b508a3ed36c9699321c6e37041836730462a9
Ricardo Oliveira <ricardo.dev1988@gmail.com>
2021-12-16 20:57:27 -0300

    Projeto compra de pizza

---
+++ assets/js/pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'assets/images/pizza.png', price:[21.90, 36.60, 40.19], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:2, name:'Calabresa', img:'assets/images/pizza2.png', price:[16.90, 25.40, 36.20], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:3, name:'Quatro Queijos', img:'assets/images/pizza3.png', price:[18.60, 27.30, 48.20], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:4, name:'Americana', img:'assets/images/pizza4.png', price:[15.90, 20.60, 30.19], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:5, name:'Sorvete', img:'assets/images/pizza5.png', price:[19.90, 26.60, 49.80], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:6, name:'Moda da Casa', img:'assets/images/pizza6.png', price:[15.90, 20.30, 35.40], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
+    {id:7, name:'Chocolate', img:'assets/images/pizza7.png', price:[18.60, 27.30, 48.20], sizes:['4 fatias', '8 fatias', '12 fatias'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'}
+];
